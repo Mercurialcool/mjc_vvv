7b67d0f16714f626b95363c8514608b3b8d5359c
Mercurial_cool <vadim.minsk61@gmail.com>
2021-02-02 17:24:12 +0300

    minor fixes

--- src/main/java/com/epam/esm/config/WebConfig.java
+++ src/main/java/com/epam/esm/config/WebConfig.java
@@ -9,6 +9,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
@@ -18,8 +21,10 @@ import javax.sql.DataSource;
 @ComponentScan(basePackages = { "com.epam.esm" })
 @PropertySource("classpath:/application.properties")
 public class WebConfig {
+
     @Autowired
     private Environment environment;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
@@ -39,6 +44,5 @@ public class WebConfig {
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate(){
         return new NamedParameterJdbcTemplate(dataSource());
     }
-
 }
 
--- src/main/java/com/epam/esm/dao/RowMapCertificateProvider.java
+++ src/main/java/com/epam/esm/dao/RowMapCertificateProvider.java
@@ -25,7 +25,7 @@ public class RowMapCertificateProvider implements RowMapper<Certificate> {
             certificate.setPrice(resultSet.getDouble(PROVIDER_PRICE));
             certificate.setDuration(resultSet.getInt(PROVIDER_DURATION));
             certificate.setCreateDate(resultSet.getTimestamp(PROVIDER_CREATE_DATE) == null ? null : resultSet.getTimestamp(PROVIDER_CREATE_DATE).toInstant());
-            certificate.setCreateDate(resultSet.getTimestamp(PROVIDER_LAST_UPDATE_DATE) == null ? null : resultSet.getTimestamp(PROVIDER_LAST_UPDATE_DATE).toInstant());
+            certificate.setLastUpdateDate(resultSet.getTimestamp(PROVIDER_LAST_UPDATE_DATE) == null ? null : resultSet.getTimestamp(PROVIDER_LAST_UPDATE_DATE).toInstant());
             return certificate;
         }
     }
